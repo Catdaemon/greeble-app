46d74579a6b2b5232dea7e77bdb4623bee769676
Carlton Downie <carl@cdownie.co.uk>
2023-07-19 14:11:13 +0100

    fix weird background rerender caused by color scheme change when backgrounded

--- src/theme/useIsDarkMode.ts
+++ src/theme/useIsDarkMode.ts
@@ -1,16 +1,29 @@
-import { useColorScheme } from 'react-native'
-import { useAppSettingsStore } from '../stores/appSettingsStore'
+import { useEffect, useState } from 'react'
+import { AppState, Appearance, ColorSchemeName } from 'react-native'
 import { shallow } from 'zustand/shallow'
+import { useAppSettingsStore } from '../stores/appSettingsStore'
 
 export default function useIsDarkMode() {
-  const colorScheme = useColorScheme()
   const [autoDarkMode, forceDarkMode] = useAppSettingsStore(
     (state) => [state.darkModeAuto, state.darkMode],
     shallow
   )
 
+  const [currentTheme, setTheme] = useState<ColorSchemeName>(
+    Appearance.getColorScheme()
+  )
+
+  useEffect(() => {
+    AppState.addEventListener('change', (nextAppState) => {
+      if (nextAppState === 'active') {
+        const theme = Appearance.getColorScheme()
+        setTheme(theme)
+      }
+    })
+  }, [])
+
   const useDarkMode =
-    (autoDarkMode && colorScheme !== 'light') ||
+    (autoDarkMode && currentTheme !== 'light') ||
     (!autoDarkMode && forceDarkMode)
 
   return useDarkMode
